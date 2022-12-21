5be2b208eba054704f0834abccff5610aab180db
neslihany <you@example.com>
2022-12-22 00:05:45 +0100

    vc

---
+++ src/C01.java
@@ -0,0 +1,8 @@
+public class C01 {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("Hersey guzel olacak");
+        System.out.println("Simdi hersey yolunda mi?");
+
+    }
+}
---
+++ src/Java.java
@@ -0,0 +1,9 @@
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("CMD cok kullanisli : )");
+        System.out.println("Bugunun sonunu gorebilecek miyiz?");
+
+
+    }
+}
